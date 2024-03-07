f3d85bf1858a8a46dc9f6d34eb4709af062ae599
Adi Bustan <dididi101@gmail.com>
2024-03-07 20:08:21 +0200

    add an upload event page

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,26 @@
-import { useState } from 'react'
+//import { useState } from 'react'
 import './App.css'
-import EventList from './components/EventList'
+//import EventList from './components/EventList'
+import UploadEvent from './components/UploadEvent'
 import NavBar from './components/navbar/NavBar'
 import { Grid } from '@mui/material'
-import SignInSide from './components/signInPage/SignIn'
+//import SignInSide from './components/signInPage/SignIn'
 
 function App() {
-  const [count, setCount] = useState(0)
+  //const [count, setCount] = useState(0)
 
   return (
     <>
-      <SignInSide/>
-      {/* <Grid container spacing={12}>
+      {/* <SignInSide/> */}
+      <Grid container spacing={4}>
         <Grid item xs={2}>
           <NavBar/>
         </Grid>
-        <Grid item xs={10} marginTop={'120px'}>
-          <EventList/>
+        <Grid item xs={10} marginTop={'80px'}>
+          {/* <EventList/> */}
+          <UploadEvent/>
         </Grid>
-      </Grid> */}
+      </Grid>
     </>
   )
 }
